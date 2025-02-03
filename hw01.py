2612f5c07eb5a758057adc5e9ab5b18d1bf0cbe1
Abhishek <rautela.abhishek@pm.me>
2025-02-02 22:29:21 -0500

    Changed name to hw01.py and passed all tests

--- hw01.py
+++ hw01.py
@@ -11,7 +11,16 @@ def egypt(n,d):
     '1/2 + 1/3 + 1/7 + 1/71 + 1/9122 + 1/141449381 + 1/100039636784966424'
     """
 
-    
+    # ceiling function (closest smallest int)
+    unit = (n+d-1) // n
+    # get next numerator and denominator ( p/q - 1/n )
+    next_n = n * unit - d
+    next_d = d * unit 
+
+    # final item in sequence
+    if next_n == 0:
+        return f"1/{unit}"
+    return f"1/{unit} + " + egypt(next_n,next_d)
 
 import doctest
 if __name__ == "__main__":
